ac807c9918f49ef6e8306a28db2f1ff129791867
Phillip Wittrock <pwittroc@google.com>
2017-05-19 19:49:03 -0700

    run controller tests as part of release

--- cmd/apiserver-builder-release/main.go
+++ cmd/apiserver-builder-release/main.go
@@ -415,6 +415,11 @@ func BuildVendor(tooldir string) string {
 			"github.com", "kubernetes-incubator", "test", "pkg", "apis", "misk", "v1beta1"))
 		cmd.Dir = pkgDir
 		RunCmd(cmd, vendordir)
+
+		cmd = exec.Command("go", "test", filepath.Join(
+			"github.com", "kubernetes-incubator", "test", "pkg", "controller", "student"))
+		cmd.Dir = pkgDir
+		RunCmd(cmd, vendordir)
 	}
 
 	return pkgDir
