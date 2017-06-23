6251d8a71c91cfdb94911257dc2c35e48b733400
Phillip Wittrock <pwittroc@google.com>
2017-06-23 10:39:44 -0700

    Use vendored generators in example

--- example/build-tools.sh
+++ example/build-tools.sh
@@ -6,36 +6,28 @@
 
 # Install generators from other repos
 if [ ! -f bin/client-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/client-gen
-    go build -o bin/client-gen k8s.io/kubernetes/cmd/libs/go2idl/client-gen
+    go build -o bin/client-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/client-gen
 fi
 if [ ! -f bin/bin/conversion-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/conversion-gen
-    go build -o bin/conversion-gen k8s.io/kubernetes/cmd/libs/go2idl/conversion-gen
+    go build -o bin/conversion-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/conversion-gen
 fi
 if [ ! -f bin/bin/deepcopy-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/deepcopy-gen
-    go build -o bin/deepcopy-gen k8s.io/kubernetes/cmd/libs/go2idl/deepcopy-gen
+    go build -o bin/deepcopy-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/deepcopy-gen
 fi
 if [ ! -f bin/openapi-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/openapi-gen
-    go build -o bin/openapi-gen k8s.io/kubernetes/cmd/libs/go2idl/openapi-gen
+    go build -o bin/openapi-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/openapi-gen
 fi
 if [ ! -f bin/defaulter-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/defaulter-gen
-    go build -o bin/defaulter-gen k8s.io/kubernetes/cmd/libs/go2idl/defaulter-gen
+    go build -o bin/defaulter-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/defaulter-gen
 fi
 if [ ! -f bin/lister-gen ] ; then
-    go get  k8s.io/kubernetes/cmd/libs/go2idl/lister-gen
-    go build -o bin/lister-gen k8s.io/kubernetes/cmd/libs/go2idl/lister-gen
+    go build -o bin/lister-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/lister-gen
 fi
 if [ ! -f bin/informer-gen ] ; then
-    go get k8s.io/kubernetes/cmd/libs/go2idl/informer-gen
-    go build -o bin/informer-gen k8s.io/kubernetes/cmd/libs/go2idl/informer-gen
+    go build -o bin/informer-gen ../vendor/k8s.io/kubernetes/cmd/libs/go2idl/informer-gen
 fi
 if [ ! -f bin/gen-apidocs ] ; then
-    go get  github.com/kubernetes-incubator/reference-docs/gen-apidocs
-    go build -o bin/gen-apidocs github.com/kubernetes-incubator/reference-docs/gen-apidocs
+    go build -o bin/gen-apidocs ../vendor/github.com/kubernetes-incubator/reference-docs/gen-apidocs
 fi
 
 # Install generators from this repo
