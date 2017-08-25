b0f76935b137a7a7d31c1df41cbf75b7cee43f93
Phillip Wittrock <pwittroc@google.com>
2017-08-24 19:11:53 -0700

    refactor example subresources into their own file

--- example/pkg/apis/miskatonic/v1beta1/university_types.go
+++ example/pkg/apis/miskatonic/v1beta1/university_types.go
@@ -24,7 +24,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apiserver/pkg/endpoints/request"
-	"k8s.io/apiserver/pkg/registry/rest"
 	//extensionsv1beta1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
@@ -130,48 +129,4 @@ func (UniversitySchemeFns) DefaultingFunction(o interface{}) {
 // conversion functions
 func (UniversitySchemeFns) GetConversionFunctions() []interface{} {
 	return []interface{}{}
-}
-
-// +genclient=true
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// +subresource-request
-type Scale struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Faculty int `json:"faculty,omitempty"`
-}
-
-var _ rest.CreaterUpdater = &ScaleUniversityREST{}
-var _ rest.Patcher = &ScaleUniversityREST{}
-
-// +k8s:deepcopy-gen=false
-type ScaleUniversityREST struct {
-	Registry miskatonic.UniversityRegistry
-}
-
-func (r *ScaleUniversityREST) Create(ctx request.Context, obj runtime.Object, includeUninitialized bool) (runtime.Object, error) {
-	scale := obj.(*Scale)
-	u, err := r.Registry.GetUniversity(ctx, scale.Name, &metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-	u.Spec.FacultySize = scale.Faculty
-	r.Registry.UpdateUniversity(ctx, u)
-	return u, nil
-}
-
-// Get retrieves the object from the storage. It is required to support Patch.
-func (r *ScaleUniversityREST) Get(ctx request.Context, name string, options *metav1.GetOptions) (runtime.Object, error) {
-	return nil, nil
-}
-
-// Update alters the status subset of an object.
-func (r *ScaleUniversityREST) Update(ctx request.Context, name string, objInfo rest.UpdatedObjectInfo) (runtime.Object, bool, error) {
-	return nil, false, nil
-}
-
-func (r *ScaleUniversityREST) New() runtime.Object {
-	return &Scale{}
 }
