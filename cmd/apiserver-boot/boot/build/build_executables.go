eb55a2a79679f9b48d5bc70580e589e0e6099afe
Phillip Wittrock <pwittroc@google.com>
2018-01-30 11:01:39 -0800

    Make generating unversioned clients,informers and listers optional

--- cmd/apiserver-boot/boot/build/build_executables.go
+++ cmd/apiserver-boot/boot/build/build_executables.go
@@ -61,6 +61,7 @@ apiserver-boot build executables --bazel --generated=false
 func AddBuildExecutables(cmd *cobra.Command) {
 	cmd.AddCommand(createBuildExecutablesCmd)
 
+	createBuildExecutablesCmd.Flags().BoolVar(&GenUnversionedClient, "gen-unversioned-client", true, "If true, generate unversioned clients.")
 	createBuildExecutablesCmd.Flags().StringVar(&vendorDir, "vendor-dir", "", "Location of directory containing vendor files.")
 	createBuildExecutablesCmd.Flags().BoolVar(&GenerateForBuild, "generate", true, "if true, generate code before building")
 	createBuildExecutablesCmd.Flags().StringVar(&goos, "goos", "", "if specified, set this GOOS")
