d606314a6088599dad9354dc5d95cf37adc53d29
yue9944882 <291271447@qq.com>
2019-08-19 21:48:06 +0800

    fixes hard-coded kubernetes-incubator pkg path

--- cmd/apiserver-builder-release/main.go
+++ cmd/apiserver-builder-release/main.go
@@ -449,15 +449,15 @@ func BuildLocalVendor(tooldir string) {
 		filepath.Join("vendor"),
 		filepath.Join(tooldir, "src"))
 	RunCmd(c, "")
-	os.MkdirAll(filepath.Join(tooldir, "src", "vendor", "github.com", "kubernetes-incubator", "apiserver-builder-alpha"), 0700)
+	os.MkdirAll(filepath.Join(tooldir, "src", "vendor", "sigs.k8s.io", "apiserver-builder-alpha"), 0700)
 	c = exec.Command("cp", "-R", "-H",
 		filepath.Join("pkg"),
-		filepath.Join(tooldir, "src", "vendor", "github.com", "kubernetes-incubator", "apiserver-builder-alpha", "pkg"))
+		filepath.Join(tooldir, "src", "vendor", "sigs.k8s.io", "apiserver-builder-alpha", "pkg"))
 	RunCmd(c, "")
 
 	c = exec.Command("bash", "-c",
 		fmt.Sprintf("find %s -name BUILD.bazel| xargs sed -i='' s'|//pkg|//vendor/sigs.k8s.io/apiserver-builder-alpha/pkg|g'",
-			filepath.Join(tooldir, "src", "vendor", "github.com", "kubernetes-incubator", "apiserver-builder-alpha", "pkg"),
+			filepath.Join(tooldir, "src", "vendor", "sigs.k8s.io", "apiserver-builder-alpha", "pkg"),
 		))
 	RunCmd(c, "")
 
