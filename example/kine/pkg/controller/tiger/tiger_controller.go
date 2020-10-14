f445023f130c39a737c9e500c42c387e1fa5303a
yue9944882 <291271447@qq.com>
2020-10-14 18:53:59 +0800

    kine example

--- example/kine/pkg/controller/tiger/tiger_controller.go
+++ example/kine/pkg/controller/tiger/tiger_controller.go
@@ -19,66 +19,23 @@ package tiger
 import (
 	"context"
 
+	"github.com/go-logr/logr"
+	ctrl "sigs.k8s.io/controller-runtime"
+
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	mysqlv1 "sigs.k8s.io/apiserver-builder-alpha/example/kine/pkg/apis/mysql/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 )
-
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
-// Add creates a new Tiger Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
-// and Start it when the Manager is Started.
-func Add(mgr manager.Manager) error {
-	return add(mgr, newReconciler(mgr))
-}
-
-// newReconciler returns a new reconcile.Reconciler
-func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileTiger{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
-}
-
-// add adds a new Controller to mgr with r as the reconcile.Reconciler
-func add(mgr manager.Manager, r reconcile.Reconciler) error {
-	// Create a new controller
-	c, err := controller.New("tiger-controller", mgr, controller.Options{Reconciler: r})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to Tiger
-	err = c.Watch(&source.Kind{Type: &mysqlv1.Tiger{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by Tiger - change this for objects you create
-	// err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
-	//	IsController: true,
-	//	OwnerType:    &mysqlv1.Tiger{},
-	// })
-	// if err != nil {
-	//	return err
-	// }
-
-	return nil
-}
 
 var _ reconcile.Reconciler = &ReconcileTiger{}
 
 // ReconcileTiger reconciles a Tiger object
 type ReconcileTiger struct {
 	client.Client
-	scheme *runtime.Scheme
+	Log    logr.Logger
+	Scheme *runtime.Scheme
 }
 
 // Reconcile reads that state of the cluster for a Tiger object and makes changes based on the state read
@@ -102,4 +59,10 @@ func (r *ReconcileTiger) Reconcile(request reconcile.Request) (reconcile.Result,
 	}
 
 	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileTiger) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&mysqlv1.Tiger{}).
+		Complete(r)
 }
