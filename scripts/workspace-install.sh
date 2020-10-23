0d408c7845d6d7b2d7a3859489fe20e6eb123f7a
yue9944882 <291271447@qq.com>
2020-10-23 15:52:59 +0800

    non-bazel build upon testing

--- scripts/workspace-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -f bazel_0.29.1-linux-x86_64.deb ]] || wget https://github.com/bazelbuild/bazel/releases/download/0.29.1/bazel_0.29.1-linux-x86_64.deb
-
-# bazel installation
-sudo dpkg -i bazel_0.29.1-linux-x86_64.deb
-
-# install apiserver-boots
-mkdir -p $(go env GOPATH)/bin/
